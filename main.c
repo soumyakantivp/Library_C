91122ed6d66b60ea06040b766350aa85633b0aed
Soumyakanti Banerjee <assfak657@gmail.com>
2020-10-14 12:35:46 +0000

    apurba_lib_1.1.1

--- main.c
+++ main.c
@@ -95,9 +95,9 @@ BOOK* readBooks(){
                 fscanf(fp,"%d",&book->id);
                 if(book->id == 0)
                         break;
-                        
+
                  while(fgetc(fp) == ' ');
-                fseek(fp,-1,SEEK_CUR); 
+                 fseek(fp,-1,SEEK_CUR); 
 
                 fscanf(fp,"%22[^\n]s",book->name);
                 fscanf(fp,"%d",&book->status);
